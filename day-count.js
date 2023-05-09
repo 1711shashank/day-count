4d9fb4d843f66e43e9e069e2b7e16ff3e8788c11
Kumar Shashank <1711shashank@gmail.com>
2023-05-09 12:55:58 +0530

    function for missing day

--- day-count.js
+++ day-count.js
@@ -1,3 +1,16 @@
+function generateDataForMissingDays(day1, day2, day3, result) {
+    const days = Object.keys(result);
+
+    if (days.includes(day1) && days.includes(day2) && !days.includes(day3)) {
+        const data1 = result[day2];
+        const data3 = result[day1];
+        result[day3] = 2 * data3 - data1;
+    }
+
+    return result;
+}
+
+
 function solution(D) {
     const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     let result = { 'Mon': 0, 'Sun': 0 };                //always given
@@ -14,41 +27,12 @@ function solution(D) {
 
     var days = Object.keys(result);
 
-    if (days.includes('Mon') && days.includes('Sun') && !days.includes('Tue')) {
-        const sun = result['Sun'];
-        const mon = result['Mon'];
-        result['Tue'] = 2 * mon - sun;
-        days = Object.keys(result);
-    }
 
-    if (days.includes('Tue') && days.includes('Mon') && !days.includes('Wed')) {
-        const mon = result['Mon'];
-        const tue = result['Tue'];
-        result['Wed'] = 2 * tue - mon;
-        days = Object.keys(result);
-    }
-
-    if (days.includes('Wed') && days.includes('Tue') && !days.includes('Thu')) {
-        const tue = result['Tue'];
-        const wed = result['Wed'];
-        result['Thu'] = 2 * wed - tue;
-        days = Object.keys(result);
-    }
-
-    if (days.includes('Thu') && days.includes('Wed') && !days.includes('Fri')) {
-        const wed = result['Wed'];
-        const thu = result['Thu'];
-        result['Fri'] = 2 * thu - wed;
-        days = Object.keys(result);
-    }
-
-    if (days.includes('Fri') && days.includes('Thu') && !days.includes('Sat')) {
-        const thu = result['Thu'];
-        const fri = result['Fri'];
-        result['Sat'] = 2 * fri - thu;
-        days = Object.keys(result);
-    }
-
+    days = Object.keys(generateDataForMissingDays('Mon', 'Sun', 'Tue', result));
+    days = Object.keys(generateDataForMissingDays('Tue', 'Mon', 'Wed', result));
+    days = Object.keys(generateDataForMissingDays('Wed', 'Tue', 'Thu', result));
+    days = Object.keys(generateDataForMissingDays('Thu', 'Wed', 'Fri', result));
+    days = Object.keys(generateDataForMissingDays('Fir', 'Thu', 'Sat', result));
 
     //sort days
     result = Object.fromEntries(Object.entries(result).sort((a, b) => {
@@ -57,6 +41,8 @@ function solution(D) {
 
     return result;
 }
+
+
 
 
 const testCase1 = {
@@ -70,13 +56,13 @@ const testCase1 = {
     '2020-01-08': -2,
 };
 const testCase2 = {
-    '2023-05-01': 6,
-    '2023-05-04': 12,
-    '2023-05-05': 14,
-    '2023-05-06': 2,
-    '2023-05-07': 4
+    '2020-01-01': 6,
+    '2020-01-04': 12,
+    '2020-01-05': 14,
+    '2020-01-06': 2,
+    '2020-01-07': 4
 };
 
 
 
-console.log(solution(testCase1));
+console.log(solution(testCase2));
